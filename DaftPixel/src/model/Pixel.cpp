dc5bd19d154d5dd53669ce44da784d93d2d9627e
unknown <daftpy@gmail.com>
2023-06-08 02:55:03 -0500

    Refactor: Fixed a small issue with operator overload statement in the Pixel class.

--- DaftPixel/src/model/Pixel.cpp
+++ DaftPixel/src/model/Pixel.cpp
@@ -44,7 +44,7 @@ void Pixel::set(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
 }
 
 std::ostream& operator<<(std::ostream& os, const Pixel& pixel) {
-    os << "Pixel(: " << static_cast<int>(pixel.red) << ", g: " << static_cast<int>(pixel.green)
+    os << "Pixel(r: " << static_cast<int>(pixel.red) << ", g: " << static_cast<int>(pixel.green)
         << ", b: " << static_cast<int>(pixel.blue) << ", a: " << static_cast<int>(pixel.alpha) << ")";
     return os;
 }
